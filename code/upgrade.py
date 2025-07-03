cc661c93c2d10505409478e0af45c9ee267ba09e
Mateus <mateusfanka13@gmail.com>
2025-07-03 19:59:12 -0300

    Initial commit

---
+++ code/upgrade.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Upgrade:
+    def __init__(self):
+        self.Window = None
+
+    def run(self, ):
+        pass
+
+    def up_strength(self, ):
+        pass
+
+    def up_life(self, ):
+        pass
--- main.py
+++ main.py
@@ -1,17 +1,10 @@
-import pygame
 import pygame as pg
 
-print('Start game!')
 pg.init()
 window = pg.display.set_mode(size=(600, 460))
 
-print('End start game!')
-
-print('Start loop!')
 while True:
-    # Check for all events
     for event in pg.event.get():
-        if event.type == pg.quit():
-            pg.quit()  # Close window
-            quit()#End pygame
-            print('Finishing loop!')
+        if event.type == pg.QUIT:
+            pg.quit()
+            quit()
